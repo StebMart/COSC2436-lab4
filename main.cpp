0e1f4a6f4715ef74d0d99af15011d13270b3be53
Steban Martinez <sdmart20@CougarNet.UH.EDU>
2023-10-11 23:41:14 -0500

    finished assignment

--- main.cpp
+++ main.cpp
@@ -1,21 +1,82 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include "pqueue.h"
 #include "ArgumentManager.h"
 
 using namespace std;
 
-int main() {
+int main(int argc, char* argv[]) {
 
-	/*ArgumentManager am(argc, argv);
+	ArgumentManager am(argc, argv);
 	ifstream input(am.get("input"));
-	ofstream output(am.get("output"));*/
+	ofstream output(am.get("output"));
 
-	ifstream input("input1.txt");
-	ofstream output("output.txt");
+	/*ifstream input("input1.txt");
+	ofstream output("output.txt");*/
+	
+	string line, action, p;
+	int priority = -1, i;
+	pqueue queue;
 
+	// reading lines from file and adding all valid imputs into priority queue
+	while (getline(input, line)) {
+		action = p = "";
+		i = 0;
+		while (line[i] != '(' && i < line.length()) {
+			action += line[i];
+			i++;
+		}
+		if (i < line.length())
+			i++;
+		while (line[i] != ')' && i < line.length()) {
+			p += line[i];
+			i++;
+		}
+		if (action != "")
+			action.pop_back();
+		if (p != "")
+			priority = stoi(p);
 
+		if (action == "open the door")
+			queue.enqueue(action, priority);
+		else if (action == "close the door")
+			queue.enqueue(action, priority);
+		else if (action == "unlock the door")
+			queue.enqueue(action, priority);
+		else if (action == "lock the door")
+			queue.enqueue(action, priority);
+	}
+	//queue.printall(); // printing for testing
+	// reading all of the items from priority queue;
+	bool open = true, locked = false;
+	string instruction = "";
 
+	while (queue.peek() != " ") {
+		instruction = queue.pop();
+		if (instruction == "open the door") {
+			if (locked == false)
+				open = true;
+		}
+		else if (instruction == "close the door") {
+			if (locked == false)
+				open = false;
+		}
+		else if (instruction == "unlock the door") {
+			locked = false;
+		}
+		else if (instruction == "lock the door") {
+			locked = true;
+		}
+	}
 
+	if (open == true)
+		output << "the door is open";
+	else if (open == false)
+		output << "the door is closed";
+
+	input.close();
+	output.close();
 
 	return 0;
 }
--- pqueue.h
+++ pqueue.h
@@ -9,16 +9,71 @@ struct node {
 	string data;
 	int priority;
 	node* next = nullptr;
+	node(string d, int p) { data = d; priority = p; }
 };
 
 class pqueue {
 public:
-
-
+	pqueue() { head = nullptr; }
+	void enqueue(string data, int priority);
+	void printall();
+	string peek();
+	string pop();
 private:
 	node* head = nullptr;
 };
 
+void pqueue::enqueue(string data, int priority) {
+	node* temp = new node(data, priority);
+
+	if (head == nullptr)
+		head = temp;
+	else if (priority < head->priority) {
+		temp->next = head;
+		head = temp;
+	}
+	else {
+		node* cu = head;
+		node* prev = nullptr;
+		while (cu != nullptr && priority >= cu->priority) {
+			prev = cu;
+			cu = cu->next;
+		}
+		if (prev != nullptr) {
+			temp->next = prev->next;
+			prev->next = temp;
+		}
+		
+	}
+}
+
+string pqueue::pop() {
+	if (head == nullptr)
+		return "";
+	string data;
+	node* temp = head;
+	data = head->data;
+	head = head->next;
+	delete temp;
+	return data;
+}
+
+string pqueue::peek() {
+	if (head == nullptr)
+		return " ";
+	return head->data;
+}
+
+void pqueue::printall() {
+	node* cu = head;
+	cout << "printing..." << endl;
+	while (cu != nullptr) {
+		cout << cu->priority << ":" << cu->data << endl;
+		cu = cu->next;
+	}
+	cout << endl;
+
+}
 
 
 
